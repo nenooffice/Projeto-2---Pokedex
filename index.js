04ae37d0687a750936080e4d5b0079d5ed818769
Neno <eugenio.bufalorodrigues@gmail.com>
2022-05-06 18:00:44 -0300

    Final

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+import {} from 'dotenv/config'
 import express from 'express';
 import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 let message = "";
